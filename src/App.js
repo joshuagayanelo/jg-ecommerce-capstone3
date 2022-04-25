4d157791c47666181a6450cb6871434ae09bf899
Joshua Ren Gayanelo <joshuagayanelo@gmail.com>
2022-04-25 21:33:11 +0800

    discussion v0.01

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+// remove the template in the module
+
+// asdf
+
+//Save an image of your favorite cartoon character inside the public folder.  
+
+// NEW SKILL: Identify how to access resources and elements from the public folder/library of th eproject.
+
+// PUBLIC -> To store all documents and modules that are shared accross all components of the app that are visible to all.
+
+// To acquire the image you want to display you may want to get from the public folder first. IMport the element 
+
+import cartoon from './tom-and-jerry2.jpg';
+    // pass down the alias that identifies the resource into our element.
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>Welcome to the course booking batch 164</h1>
+    <h5>This is our project in React</h5>
+    <h6>Come visit our website.</h6>
+
+    <img src={cartoon} alt="image not found" />
+    <h3>This is my favorite Cartoon Character.</h3>
+
+  </div>
   );
 }
 
