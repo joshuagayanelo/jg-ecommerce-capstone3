70939e0b9d54e1811001d2003b1f88334810b00f
Joshua Ren Gayanelo <joshuagayanelo@gmail.com>
2022-04-27 19:54:28 +0800

    done working on the banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -13,7 +13,7 @@ import { Col, Row } from 'react-bootstrap';
 export default function Banner() {
 	return(
        <Row>
-			<Col>
+			<Col className="p-5 m-3">
 			   <h1>Welcome to the App</h1>
 			   <p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit.</p>
 			</Col>          
