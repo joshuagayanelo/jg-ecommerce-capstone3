bf60e3a6c0a5317994149c325c9c071e2d40a96a
jgayanelo <joshuagayanelo@gmail.com>
2022-04-28 20:01:44 +0800

    produced a clean copy of the main project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-//get the component that you want to display in the browser by specifying the location and the file you want to retrieve and use.
+
 import Banner from './components/Banner';
 import AppNavBar from './components/AppNavBar';
 import Highlights from './components/Highlights';
 import CourseCard from './components/CourseCard';
 
-//Create a JSX element for the variable that describes the component.
-
-//refactor the template to have a much conside script. 
-//export default -> this keyword is used to 'expose' the component/function across all modules in the application. 
 export default function App() {
   return (
       <div>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,12 @@
-//Lets create our first reusable component.
-//The purpose of this component is to be a header across the pages inside our application.
+import { Col, Row } from 'react-bootstrap'; 
 
-
-//1. identify and prepare the needed ingredients.
-  //main ingredient: grid-system of bootstrap
-//the 'import' => keyword is used to acquire the components/modules needed.
-//we are trying to acquire multiple components inside a single statement, so what we need to do is to wrap the components inside a container.
-import { Col as theCol, Row as theRow } from 'react-bootstrap'; 
-
-//2. Create a function that will describe how the components will look like.
-//3. expose the component so that it will become reusable for other modules inside the app. 
 export default function Banner() {
 	return(
-       <theRow>
-			<theCol className="p-5 m-3">
+       <Row>
+			<Col className="p-5 m-3">
 			   <h1>Welcome to the App</h1>
 			   <p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit.</p>
-			</theCol>          
-       </theRow>
+			</Col>          
+       </Row>
 	);
 };
