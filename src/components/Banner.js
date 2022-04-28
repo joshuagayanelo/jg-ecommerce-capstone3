e8b84f902320f4c7d4c8092bb073c5c88b950bf6
jgayanelo <joshuagayanelo@gmail.com>
2022-04-28 18:11:23 +0800

    done demonstrating 2 import methods in React

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,17 +6,17 @@
   //main ingredient: grid-system of bootstrap
 //the 'import' => keyword is used to acquire the components/modules needed.
 //we are trying to acquire multiple components inside a single statement, so what we need to do is to wrap the components inside a container.
-import { Col, Row } from 'react-bootstrap'; 
+import { Col as theCol, Row as theRow } from 'react-bootstrap'; 
 
 //2. Create a function that will describe how the components will look like.
 //3. expose the component so that it will become reusable for other modules inside the app. 
 export default function Banner() {
 	return(
-       <Row>
-			<Col className="p-5 m-3">
+       <theRow>
+			<theCol className="p-5 m-3">
 			   <h1>Welcome to the App</h1>
 			   <p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit.</p>
-			</Col>          
-       </Row>
+			</theCol>          
+       </theRow>
 	);
 };
