bca83e453e5371f39e4542a1f1020d1991d6a53b
Joshua Ren Gayanelo <joshuagayanelo@gmail.com>
2022-05-02 18:41:46 +0800

    currently working on our login page

--- src/components/AppNavBar.js
+++ src/components/AppNavBar.js
@@ -1,4 +1,4 @@
-import {Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export default function AppNavBar() {
@@ -19,3 +19,36 @@ export default function AppNavBar() {
 		</Navbar>
 	)
 };
+
+
+// import { Navbar, Nav } from 'react-bootstrap';
+
+// //[SECTION] New Task: Modify the Navbar to make it a 'funcitonal' navigation component.
+
+// //1. Acquire the Link utility from 'react-router-dom'.
+// import { Link } from 'react-router-dom';
+// 	//Link -> will allow us to redirect a suer to a specific path/location.
+// 		//the link component will come with a required single attribute:
+// 			//to -> is used to assign a designated location or path to redirect the user.
+
+// //Task: Modify the appearance of the Link components so that it will blend accordingly to the skin of the navbar instead of looking like an actual anchor tag.
+// 	//=> solution for this is to use utility classes 
+
+// export default function AppNavBar() {
+// 	return(
+// 		<Navbar>
+// 		<Navbar.Brand>
+// 			BOOKAS
+// 		</Navbar.Brand>
+// 		<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+// 		<Navbar.Collapse id="basic-navbar-nav">
+// 			<Nav className="ml-auto">
+// 				<Link className="nav-link" to="/"> Home </Link>
+// 				<Link className="nav-link" to="/register"> Register </Link>
+// 				<Link className="nav-link" to="/login"> Login </Link>
+// 				<Link className="nav-link" to="/courses"> Courses </Link>
+// 			</Nav>
+// 		</Navbar.Collapse>
+// 		</Navbar>
+// 	)
+// };
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,16 +1,52 @@
 import Banner from './../components/Banner';
-import {Container} from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 
 const bannerLogin = {
 	title: 'Sign in to your account here',
 	content: 'View your profile when you sign in.'
 }
 
-export default function () {
+export default function Login() {
 	return(
 		<Container>
 			<Banner bannerData = {bannerLogin} />
 			<h1 className="text-center mt-3">Login Page</h1>
+			<Form className="mt-5">
+				{/*Email address*/}
+				<Form.Group>
+					<Form.Label>Email:</Form.Label>	
+					<Form.Control 
+						type="email"
+						placeholder="Enter your email address here"
+					/>	
+				</Form.Group>
+				{/*Password*/}
+				<Form.Group>	
+					<Form.Label>Password:</Form.Label>	
+					<Form.Control 
+						type="password"
+						placeholder="Please enter your password here"
+					/>
+				</Form.Group>
+
+				<Button 
+					className="btn-block" 
+					variant="success"
+					type="submit"
+				>
+					Login
+				</Button>
+
+				<Button
+					className="btn-block"
+					variant="secondary"
+					disabled
+				>
+					Login
+				</Button>
+
+			</Form>
+
 		</Container>		
 	);
-}
+};	
